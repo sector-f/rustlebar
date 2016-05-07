027103fc5ee1422fd63bac5a692c4844ee53bf38
Adam <adamsimeth@gmail.com>
2016-05-07 10:23:58 -0400

    It works now

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 use std::io::{stderr,BufRead, BufReader, Write};
 use std::process::{Command,exit,Stdio};
 use std::sync::mpsc::{channel, Sender};
-use std::thread;
+use std::thread::{self, sleep};
+use std::time::Duration;
 
 const OCCUPIED_ICON: &'static str = "";
 const FREE_ICON: &'static str = "";
 
-// enum Workspace {
-//     OccupiedFocused,
-//     OccupiedUnfocused,
-//     FreeFocused,
-//     FreeUnfocused,
-//     UrgentFocused,
-//     UrgentUnfocused,
-// }
+enum Update {
+    Workspaces(String),
+    Time(String),
+    Title(String),
+}
 
-// enum Update {
-//     Workspaces(String),
-//     Time(String),
-//     Title(String),
-// }
+fn workspace_info(sender: Sender<Update>) {
+    // let mut old_message: Vec<String> = vec!["".to_owned()];
 
-fn workspace_info(sender: Sender<String>) {
     let output = match Command::new("bspc").arg("subscribe").arg("report").stdout(Stdio::piped()).spawn() {
         Ok(out) => out,
         Err(_) => {
@@ -32,64 +26,140 @@ fn workspace_info(sender: Sender<String>) {
 
     let reader = BufReader::new(output.stdout.unwrap());
     for line in reader.lines() {
-        let _ = sender.send(format!("%{{c}}"));
-        let _ = sender.send(format!("%{{A4:bspc desktop -f prev:}}%{{A5:bspc desktop -f next:}}"));
+        let mut message: Vec<String> = Vec::new();
+        message.push(format!(" %{{l}}"));
+        message.push(format!("%{{A4:bspc desktop -f prev:}}%{{A5:bspc desktop -f next:}}"));
+
         let line = line.unwrap();
         let line_vector = line.split(":").collect::<Vec<&str>>();
 
         for line in &line_vector {
-            let workspace = &line[1..];
-
-            let _ = sender.send(format!("%{{A:bspc desktop -f {}:}}", workspace));
-
             match &line[0..1] {
                 // Occupied focused
                 "O" => {
-                    let _ = sender.send(format!("%{{F#FFF6F9FF}} {} %{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FFF6F9FF}} {}%{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 // Occupied unfocused
                 "o" => {
-                    let _ = sender.send(format!("%{{F#FFA3A6AB}} {} %{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FFA3A6AB}} {}%{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 // Free focused
                 "F" => {
-                    let _ = sender.send(format!("%{{F#FFF6F9FF}} {} %{{F-}}", FREE_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FFF6F9FF}} {}%{{F-}}", FREE_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 // Free unfocused
                 "f" => {
-                    let _ = sender.send(format!("%{{F#FF6F7277}} {} %{{F-}}", FREE_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FF6F7277}} {}%{{F-}}", FREE_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 // Urgent focused
                 "U" => {
-                    let _ = sender.send(format!("%{{F#FF916255}} {} %{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FF916255}} {}%{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 // Urgent unfocused
                 "u" => {
-                    let _ = sender.send(format!("%{{F#FF543B3B}} {} %{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A:bspc desktop -f {}:}}", &line[1..]));
+                    message.push(format!("%{{F#FF543B3B}} {}%{{F-}}", OCCUPIED_ICON));
+                    message.push(format!("%{{A}}"));
                 }
 
                 _ => {}
             }
-            let _ = sender.send(format!("%{{A}}"));
         }
-        let _ = sender.send(format!("%{{A}}%{{A}}"));
-        let _ = sender.send(format!("\n"));
+        message.push(format!("%{{A}}%{{A}}"));
+
+        let _ = sender.send(Update::Workspaces(message.join("")));
+    }
+}
+
+fn title(sender: Sender<Update>) {
+    let output = match Command::new("xtitle").arg("-s").arg("-i").arg("-t").arg("100").stdout(Stdio::piped()).spawn() {
+        Ok(out) => out,
+        Err(_) => {
+            let _ = stderr().write("Failed to run 'xtitle'\n".as_bytes()).unwrap();
+            exit(1);
+        },
+    };
+
+    let reader = BufReader::new(output.stdout.unwrap());
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let _ = sender.send(Update::Title(format!("%{{c}}{}", line)));
+    }
+}
+
+fn time(sender: Sender<Update>) {
+    let output = match Command::new("clock").arg("-s").arg("-i").arg("1").arg("-f").arg("%I:%M %p").stdout(Stdio::piped()).spawn() {
+        Ok(out) => out,
+        Err(_) => {
+            let _ = stderr().write("Failed to run 'clock'\n".as_bytes()).unwrap();
+            exit(1);
+        },
+    };
+
+    let reader = BufReader::new(output.stdout.unwrap());
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let _ = sender.send(Update::Time(format!("%{{r}}%{{B#FF665B5B}} {} %{{B-}}", line)));
     }
 }
 
 fn main() {
-    let (sender, receiver) = channel::<String>();
+    let (sender, receiver) = channel::<Update>();
 
+    let mut workspace_message: String = "".to_owned();
+    let mut title_message: String = "".to_owned();
+    let mut time_message: String = "".to_owned();
+    let mut message: String = "".to_owned();
+
+    let sender_clone = sender.clone();
     let _ = thread::spawn(move || {
-        workspace_info(sender.clone());
+        workspace_info(sender_clone);
+    });
+
+    let sender_clone = sender.clone();
+    let _ = thread::spawn(move || {
+        title(sender_clone);
+    });
+
+    let sender_clone = sender.clone();
+    let _ = thread::spawn(move || {
+        time(sender_clone);
     });
 
     for line in receiver.iter() {
-        print!("{}", line);
+        match line {
+            Update::Workspaces(info) => {
+                workspace_message = info;
+            }
+
+            Update::Title(title) => {
+                title_message = title;
+            }
+
+            Update::Time(time) => {
+                time_message = time;
+            }
+        }
+
+        if message != workspace_message.to_owned() + &title_message + &time_message {
+            message = workspace_message.to_owned() + &title_message + &time_message;
+            sleep(Duration::from_millis(5));
+            println!("{}", message);
+        }
     }
 }
